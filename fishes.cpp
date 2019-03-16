467c9651178918af4103092b71e08e4c5483486d
UserHyuser <drachevskii@yandex.ru>
2019-03-16 13:38:25 +0700

    added func

--- fishes.cpp
+++ fishes.cpp
@@ -9,6 +9,13 @@ namespace simple_animals {
   {
     ifst >> t.place;
   }
+  int LenghtName(fish &t, char name[20]) {
+	  int arrayLength = 0;
+	  for (int i = 0; name[i]; i++) {
+		  arrayLength++;
+	  }
+	  return arrayLength;
+  }
   void Out(fish &t, char name[20] ,ofstream &ofst)
   {
 	  if (t.place == 0) {
